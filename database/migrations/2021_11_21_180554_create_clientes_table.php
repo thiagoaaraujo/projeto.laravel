cebd6449576fc9a5c42c7c8fadf9a784f41fab5b
thiagoaaraujo <thiagoaaraujo@unipam.edu.br>
2021-11-21 18:57:28 -0300

    Migrations parte1

---
+++ database/migrations/2021_11_21_180554_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 50);
+            $table->int('numFicha', 3)->nullable();
+            $table->string('endereco', 80);
+            $table->string('comorbidade', 60);
+            $table->string('telefone', 20);
+            $table->string('telefone2', 20);
+            $table->int('estratificacao', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
